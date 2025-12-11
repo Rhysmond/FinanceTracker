e752b9357786bf5c22d311f155f553a52d5762bf
Rhysmond <E1406512@u.nus.edu>
2025-12-11 22:30:58 +0800

    Finish backend part 1

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type { Express } from "express";
 import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records.ts"
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -16,6 +17,8 @@ mongoose
     .connect(mongoURI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("Failed to connect to mongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
